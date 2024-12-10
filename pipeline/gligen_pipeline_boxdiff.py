f30e929addfec994e5e55ca1f24b7bfa494044c5
lionl1106 <66839621+lionl1106@users.noreply.github.com>
2024-12-11 06:36:59 +0800

    Add files via upload

--- pipeline/gligen_pipeline_boxdiff.py
+++ pipeline/gligen_pipeline_boxdiff.py
@@ -85,38 +85,27 @@ class BoxDiffPipeline(StableDiffusionGLIGENPipeline):
 
         if prompt_embeds is None:
             text_inputs = self.tokenizer(
-                prompt,
+                prompt, 
                 padding="max_length",
-                max_length=self.tokenizer.model_max_length,
-                truncation=True,
-                return_tensors="pt",
-            )
-            text_input_ids = text_inputs.input_ids
-            untruncated_ids = self.tokenizer(prompt, padding="longest", return_tensors="pt").input_ids
+                max_length=512,
+                truncation=False,
+                return_tensors="pt"
+              )
 
-            if untruncated_ids.shape[-1] >= text_input_ids.shape[-1] and not torch.equal(
-                text_input_ids, untruncated_ids
-            ):
-                removed_text = self.tokenizer.batch_decode(
-                    untruncated_ids[:, self.tokenizer.model_max_length - 1 : -1]
-                )
-                logger.warning(
-                    "The following part of your input was truncated because CLIP can only handle sequences up to"
-                    f" {self.tokenizer.model_max_length} tokens: {removed_text}"
-                )
+            text_input_ids = text_inputs.input_ids.to("cuda")
 
             if hasattr(self.text_encoder.config, "use_attention_mask") and self.text_encoder.config.use_attention_mask:
                 attention_mask = text_inputs.attention_mask.to(device)
             else:
                 attention_mask = None
 
-            prompt_embeds = self.text_encoder(
-                text_input_ids.to(device),
-                attention_mask=attention_mask,
-            )
-            prompt_embeds = prompt_embeds[0]
+            concat_embeds = []
+            for i in range(0, text_input_ids.shape[-1], self.tokenizer.model_max_length):
+                concat_embeds.append(
+                    self.text_encoder(text_input_ids[:, i: i + self.tokenizer.model_max_length], attention_mask=attention_mask)[0]
+                  )
 
-        prompt_embeds = prompt_embeds.to(dtype=self.text_encoder.dtype, device=device)
+            prompt_embeds = torch.cat(concat_embeds, dim=1)
 
         bs_embed, seq_len, _ = prompt_embeds.shape
         # duplicate text embeddings for each generation per prompt, using mps friendly method
@@ -144,25 +133,29 @@ class BoxDiffPipeline(StableDiffusionGLIGENPipeline):
             else:
                 uncond_tokens = negative_prompt
 
-            max_length = prompt_embeds.shape[1]
+            #max_length = prompt_embeds.shape[1]
             uncond_input = self.tokenizer(
                 uncond_tokens,
                 padding="max_length",
-                max_length=max_length,
-                truncation=True,
+                max_length=512,
+                truncation=False,
                 return_tensors="pt",
             )
+
+            uncond_input_idx = uncond_input.input_ids.to("cuda")
 
             if hasattr(self.text_encoder.config, "use_attention_mask") and self.text_encoder.config.use_attention_mask:
                 attention_mask = uncond_input.attention_mask.to(device)
             else:
                 attention_mask = None
 
-            negative_prompt_embeds = self.text_encoder(
-                uncond_input.input_ids.to(device),
-                attention_mask=attention_mask,
-            )
-            negative_prompt_embeds = negative_prompt_embeds[0]
+            concat_embeds = []
+            for i in range(0, uncond_input_idx.shape[-1], self.tokenizer.model_max_length):
+                concat_embeds.append(
+                    self.text_encoder(uncond_input_idx[:, i: i + self.tokenizer.model_max_length], attention_mask=attention_mask)[0]
+                  )
+
+            negative_prompt_embeds = torch.cat(concat_embeds, dim=1)
 
         if do_classifier_free_guidance:
             # duplicate unconditional embeddings for each generation per prompt, using mps friendly method
