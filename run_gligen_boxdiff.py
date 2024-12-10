00a7ca2e991fe10bb7aacc6571a1f505c85567c1
lionl1106 <66839621+lionl1106@users.noreply.github.com>
2024-12-11 07:07:58 +0800

    Add files via upload

--- run_gligen_boxdiff.py
+++ run_gligen_boxdiff.py
@@ -1,4 +1,6 @@
-
+from dataclasses import field
+import random
+import os
 import pprint
 from typing import List
 
@@ -7,11 +9,11 @@ import torch
 from PIL import Image
 from config import RunConfig
 from pipeline.gligen_pipeline_boxdiff import BoxDiffPipeline
-from utils import ptp_utils, vis_utils
+from utils import ptp_utils
 from utils.ptp_utils import AttentionStore
 
 import numpy as np
-from utils.drawer import draw_rectangle, DashedImageDraw
+from utils.drawer import draw_rectangle
 
 import warnings
 warnings.filterwarnings("ignore", category=UserWarning)
@@ -22,7 +24,7 @@ def load_model():
     stable_diffusion_version = "gligen/diffusers-generation-text-box"
     # If you cannot access the huggingface on your server, you can use the local prepared one.
     # stable_diffusion_version = "../../packages/diffusers/gligen_ckpts/diffusers-generation-text-box"
-    stable = BoxDiffPipeline.from_pretrained(stable_diffusion_version, revision="fp16", torch_dtype=torch.float16).to(device)
+    stable = BoxDiffPipeline.from_pretrained(stable_diffusion_version, torch_dtype=torch.float16, safety_checker = None).to(device)
 
     return stable
 
@@ -31,7 +33,6 @@ def get_indices_to_alter(stable, prompt: str) -> List[int]:
     token_idx_to_word = {idx: stable.tokenizer.decode(t)
                          for idx, t in enumerate(stable.tokenizer(prompt)['input_ids'])
                          if 0 < idx < len(stable.tokenizer(prompt)['input_ids']) - 1}
-    pprint.pprint(token_idx_to_word)
     token_indices = input("Please enter the a comma-separated list indices of the tokens you wish to "
                           "alter (e.g., 2,5): ")
     token_indices = [int(i) for i in token_indices.split(",")]
@@ -43,23 +44,26 @@ def run_on_prompt(prompt: List[str],
                   model: BoxDiffPipeline,
                   controller: AttentionStore,
                   token_indices: List[int],
+                  bbox: List[int],
+                  gligen_phrases: List[str],
                   seed: torch.Generator,
                   config: RunConfig) -> Image.Image:
+                                                                                #這邊需要加box這個參數進來
     if controller is not None:
         ptp_utils.register_attention_control(model, controller)
 
     gligen_boxes = []
-    for i in range(len(config.bbox)):
-        x1, y1, x2, y2 = config.bbox[i]
+    for i in range(len(bbox)):
+        x1, y1, x2, y2 = bbox[i]
         gligen_boxes.append([x1/512, y1/512, x2/512, y2/512])
 
     outputs = model(prompt=prompt,
                     attention_store=controller,
-                    indices_to_alter=token_indices,
+                    indices_to_alter=token_indices,                             #這邊需要修改
                     attention_res=config.attention_res,
                     guidance_scale=config.guidance_scale,
-                    gligen_phrases=config.gligen_phrases,
-                    gligen_boxes=gligen_boxes,
+                    gligen_phrases=gligen_phrases,
+                    gligen_boxes=gligen_boxes,                                  #這邊需要修改
                     gligen_scheduled_sampling_beta=0.3,
                     generator=seed,
                     num_inference_steps=config.n_inference_steps,
@@ -81,40 +85,104 @@ def run_on_prompt(prompt: List[str],
 @pyrallis.wrap()
 def main(config: RunConfig):
     stable = load_model()
-    token_indices = get_indices_to_alter(stable, config.prompt) if config.token_indices is None else config.token_indices
+    
+    category_count = {"person" : 0, "ear" : 0, "earmuffs" : 0, "face" : 0, "face-guard" : 0, "face-mask-medical" : 0, "foot" : 0, "tools" : 0, 
+    "glasses" : 0, "gloves" : 0, "helmet" : 0, "hands" : 0, "head" : 0, "medical-suit" : 0, "shoes" : 0, "safety-suit" : 0, "safety-vest" : 0}
+    
+    controller = AttentionStore()
+    controller.num_uncond_att_layers = -16
+    #prompt_output_path = config.output_path / str(len(os.listdir(config.output_path)) + 1)
+    prompt_output_path = config.output_path
+    prompt_output_path.mkdir(exist_ok=True, parents=True)
 
-    if len(config.bbox[0]) == 0:
-        config.bbox = draw_rectangle()
+    for info in config.info:
+        tokens = [stable.tokenizer.decode(code) for code in stable.tokenizer(info['prompt'])['input_ids']]
+                
+        token_indices, bbox, gligen_phrases = [], [], []
+        for i in range(len(tokens)):
+            if tokens[i] == "ear":
+                if tokens[i + 1]  == "mu":
+                    if "earmuffs" in info  and category_count["earmuffs"] < len(info["earmuffs"]):
+                        token_indices.append(i+1)
+                        gligen_phrases.append("earmuffs")
+                        bbox.append(info["earmuffs"][category_count["earmuffs"]])
+                        category_count["earmuffs"] += 1
+                elif "ear" in info  and category_count["ear"] < len(info["ear"]):
+                    token_indices.append(i)
+                    gligen_phrases.append("ear")
+                    bbox.append(info["ear"][category_count["ear"]])
+                    category_count["ear"] += 1
+                    
+            elif tokens[i] == "face":
+                if tokens[i + 1] != "-":
+                    if "face" in info  and category_count["face"] < len(info["face"]):
+                        token_indices.append(i)
+                        gligen_phrases.append("face")
+                        bbox.append(info["face"][category_count["face"]])
+                        category_count["face"] += 1
+                elif tokens[i + 2] == "guard":
+                    if  "face-guard" in info  and category_count["face-guard"] < len(info["face-guard"]):
+                        token_indices.append(i+2)
+                        gligen_phrases.append("face-guard")
+                        bbox.append(info["face-guard"][category_count["face-guard"]])
+                        category_count["face-guard"] += 1
+                elif "face-mask-medical" in info  and category_count["face-mask-medical"] < len(info["face-mask-medical"]):
+                    token_indices.append(i+2)
+                    gligen_phrases.append("face-mask-medical")
+                    bbox.append(info["face-mask-medical"][category_count["face-mask-medical"]])
+                    category_count["face-mask-medical"] += 1
+                    
+            elif tokens[i] == "medical":
+                if "medical-suit" in info  and category_count["medical-suit"] < len(info["medical-suit"]):
+                    token_indices.append(i+2)
+                    gligen_phrases.append("medical-suit")
+                    bbox.append(info["medical-suit"][category_count["medical-suit"]])
+                    category_count["medical-suit"] += 1
+                
+            elif tokens[i]  == "safety":
+                if tokens[i + 1]  != "-":
+                    continue
+                if tokens[i + 2] == "suit":
+                    if "safety-suit" in info  and category_count["safety-suit"] < len(info["safety-suit"]):
+                        token_indices.append(i+2)
+                        gligen_phrases.append("safety-suit")
+                        bbox.append(info["safety-suit"][category_count["safety-suit"]])
+                        category_count["safety-suit"] += 1
+                elif "safety-vest" in info  and category_count["safety-vest"] < len(info["safety-vest"]):
+                    token_indices.append(i+2)
+                    gligen_phrases.append("safety-vest")
+                    bbox.append(info["safety-vest"][category_count["safety-vest"]])
+                    category_count["safety-vest"] += 1
+                    
+            elif tokens[i] in info and category_count[tokens[i]] < len(info[tokens[i]]):
+                token_indices.append(i)
+                gligen_phrases.append(tokens[i])
+                bbox.append(info[tokens[i]][category_count[tokens[i]]])
+                category_count[tokens[i]] += 1
+                
+        config.bbox = bbox
+        config.prompt = info['prompt']
+                
+        print(f"Current iamge is : {info['name']}")
 
-    images = []
-    for seed in config.seeds:
-        print(f"Current seed is : {seed}")
-        g = torch.Generator('cuda').manual_seed(seed)
-        controller = AttentionStore()
-        controller.num_uncond_att_layers = -16
-        image = run_on_prompt(prompt=config.prompt,
-                              model=stable,
-                              controller=controller,
-                              token_indices=token_indices,
-                              seed=g,
-                              config=config)
-        prompt_output_path = config.output_path / config.prompt[:100]
-        prompt_output_path.mkdir(exist_ok=True, parents=True)
-        image.save(prompt_output_path / f'{seed}.png')
-        images.append(image)
-
-        canvas = Image.fromarray(np.zeros((image.size[0], image.size[0], 3), dtype=np.uint8) + 220)
-        draw = DashedImageDraw(canvas)
-
-        for i in range(len(config.bbox)):
-            x1, y1, x2, y2 = config.bbox[i]
-            draw.dashed_rectangle([(x1, y1), (x2, y2)], dash=(5, 5), outline=config.color[i], width=5)
-        canvas.save(prompt_output_path / f'{seed}_canvas.png')
-
-    # save a grid of results across all seeds
-    joined_image = vis_utils.get_image_grid(images)
-    joined_image.save(config.output_path / f'{config.prompt}.png')
-
-
+        config.seeds = [random.randint(0, 1000000)]  # Generates a list of 5 random seeds
+        for seed in config.seeds:
+            print(f"Current seed is : {seed}")
+            g = torch.Generator('cuda').manual_seed(seed)
+            
+            image = run_on_prompt(prompt=info['prompt'],
+                                  gligen_phrases=gligen_phrases,
+                                  model=stable,
+                                  controller=controller,
+                                  token_indices=token_indices,
+                                  bbox=bbox,
+                                  seed=g,
+                                  config=config)
+            
+            image.save(prompt_output_path / info['name'])
+                
+        for category in category_count:
+            category_count[category] = 0
+            
 if __name__ == '__main__':
     main()
