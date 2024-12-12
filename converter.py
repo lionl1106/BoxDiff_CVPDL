0761ee5b57d9eb4cf8072ab3b7977f04d67cf971
lionl1106 <66839621+lionl1106@users.noreply.github.com>
2024-12-13 06:49:33 +0800

    Update converter.py

--- converter.py
+++ converter.py
@@ -17,12 +17,13 @@ for image in images:
     dict = {"name" : image['image'], "prompt" : prompts[image['image']] + " There are "}
         
     for i, obj in enumerate(image['labels']):
+        obj = obj.lower()
         dict.setdefault(obj, []).append(image['bboxes'][i])
         dict['prompt'] += f"{obj}" if i == 0 else f", {obj}"
 
     dict['prompt'] += ". 8k resolution, highly detailed, ultra-realistic, anatomically correct, well-defined hands and fingers."
     list.append(dict)
 
-os.mkdir(path_out, exist_ok=True, parents=True)
+os.makedirs(path_out, exist_ok=True)
 with open(f'{path_out}/prompts_with_info.json', 'w') as f:
     json.dump(list, f)
